a9a305b19a9e70c76a6f89e047c3ec2206bd38af
Sergey Klitko <sergey.klitko@netzlabor-coding.de>
2018-10-30 15:06:28 +0200

    fix ua

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
-import * as loadScript from 'simple-load-script'
-import 'babel-polyfill'
-
 import Layout from '../components/Layout'
 import FirstScreen from '../components/screens/FirstScreen'
 import SecondScreen from '../components/screens/SecondScreen'
@@ -15,33 +12,6 @@ import SixthScreen from '../components/screens/SixthScreen'
 import SevethScreen from '../components/screens/SeventhScreen'
 
 export default class IndexPage extends React.Component {
-  async componentDidMount() {
-    await loadScript('libs/modernizr-2.8.3.min.js')
-    await loadScript('libs/modernizr-custom.js')
-    await loadScript('//code.jquery.com/jquery-3.3.1.min.js')
-    await loadScript('libs/jquery.mobile.custom.min.js')
-    await loadScript(
-      '//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js'
-    )
-    await loadScript(
-      '//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js'
-    )
-    await loadScript(
-      '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'
-    )
-    await loadScript(
-      '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
-    )
-    await loadScript(
-      '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'
-    )
-    await loadScript(
-      '//cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js'
-    )
-
-    await loadScript('js/scripts.min.js')
-  }
-
   render() {
     const { data } = this.props
     const { frontmatter } = data.markdownRemark
