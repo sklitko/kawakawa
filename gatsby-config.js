897ee5962557fd1b02ea6cc32e248bca470486a7
Sergey Klitko <sergey.klitko@netzlabor-coding.de>
2019-02-15 17:38:01 +0200

    add config fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,18 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      options: {
+        fonts: [
+          {
+            family: 'Open Sans',
+            variants: ['300','400','600','700'],
+            subsets: ['cyrillic']
+          },
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
